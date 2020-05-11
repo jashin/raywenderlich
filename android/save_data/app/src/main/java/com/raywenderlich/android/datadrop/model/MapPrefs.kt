8b8dcb1cd757efdfd731c0665e52ab5b9eb1512e
steven.zhan <steven.zhan@everbridge.com>
2020-05-10 21:34:31 -0400

    sharedpref

---
+++ android/save_data/app/src/main/java/com/raywenderlich/android/datadrop/model/MapPrefs.kt
@@ -0,0 +1,18 @@
+package com.raywenderlich.android.datadrop.model
+
+import android.preference.PreferenceManager
+import com.raywenderlich.android.datadrop.app.DataDropApplication
+
+
+object MapPrefs {
+    private const val KEY_MARKER_COLOR = "KEY_MARKER_COLOR"
+
+    private fun sharedPrefs() = PreferenceManager.getDefaultSharedPreferences(DataDropApplication.getAppContext())
+
+    fun saveMarkerColor(markerColor: String) {
+        val editor = sharedPrefs().edit()
+        editor.putString(KEY_MARKER_COLOR, markerColor).apply()
+    }
+
+    fun getMarkerColor(): String = sharedPrefs().getString(KEY_MARKER_COLOR, "Red")
+}
