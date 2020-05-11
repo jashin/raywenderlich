f7a8ac6581d728f2c70ceb3f5b521616c6478b00
steven.zhan <steven.zhan@everbridge.com>
2020-05-11 16:54:44 -0400

    save file

--- android/save_data/app/src/main/java/com/raywenderlich/android/datadrop/model/DropTypeAdapter.kt
+++ android/save_data/app/src/main/java/com/raywenderlich/android/datadrop/model/DropTypeAdapter.kt
@@ -29,26 +29,45 @@
  *
  */
 
-package com.raywenderlich.android.datadrop.app
+package com.raywenderlich.android.datadrop.model
 
-import com.raywenderlich.android.datadrop.model.DropRepository
-import com.raywenderlich.android.datadrop.model.FileRepository
-import com.raywenderlich.android.datadrop.model.InMemoryRepository
-import com.raywenderlich.android.datadrop.model.SharedPrefsRepository
-import com.raywenderlich.android.datadrop.ui.droplist.DropListContract
-import com.raywenderlich.android.datadrop.ui.droplist.DropListPresenter
-import com.raywenderlich.android.datadrop.ui.map.MapContract
-import com.raywenderlich.android.datadrop.ui.map.MapPresenter
+import com.google.android.gms.maps.model.LatLng
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
+import java.io.IOException
 
-object Injection {
 
-  private fun provideDropRepository(): DropRepository = FileRepository
+class DropTypeAdapter : TypeAdapter<Drop>() {
 
-  fun provideMapPresenter(view: MapContract.View): MapContract.Presenter {
-    return MapPresenter(provideDropRepository(), view)
+  @Throws(IOException::class)
+  override fun read(reader: JsonReader): Drop {
+    var latitude = 0.0
+    var longitude = 0.0
+    var dropMessage = ""
+    var id = ""
+
+    reader.beginObject()
+    while (reader.hasNext()) {
+      when (reader.nextName()) {
+        "latitude" -> latitude = reader.nextDouble()
+        "longitude" -> longitude = reader.nextDouble()
+        "dropMessage" -> dropMessage = reader.nextString()
+        "id" -> id = reader.nextString()
+      }
+    }
+    reader.endObject()
+
+    return Drop(LatLng(latitude, longitude), dropMessage, id)
   }
 
-  fun provideDropListPresenter(view: DropListContract.View): DropListContract.Presenter {
-    return DropListPresenter(provideDropRepository(), view)
+  @Throws(IOException::class)
+  override fun write(out: JsonWriter, drop: Drop) {
+    out.beginObject()
+    out.name("latitude").value(drop.latLng.latitude)
+    out.name("longitude").value(drop.latLng.longitude)
+    out.name("dropMessage").value(drop.dropMessage)
+    out.name("id").value(drop.id)
+    out.endObject()
   }
 }
